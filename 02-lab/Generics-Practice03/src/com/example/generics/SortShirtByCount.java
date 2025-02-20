460a2efb9df17667f6e01f1ec3131c557d64594f
Kesha <kesha@gmail.com>
2025-02-20 13:38:55 +0500

    implementation of sort methods

--- 02-lab/Generics-Practice03/src/com/example/generics/SortShirtByCount.java
+++ 02-lab/Generics-Practice03/src/com/example/generics/SortShirtByCount.java
@@ -4,9 +4,9 @@ import java.util.Comparator;
 
 public class SortShirtByCount implements Comparator<Shirt> {
 
-    public int compare(Shirt s1, Shirt s2) {
-            
-        return 0;
-
-    }
+    public int compare(Shirt s1, Shirt s2){
+        Long c1 = Long.valueOf(s1.getCount());
+        Long c2 = Long.valueOf(s2.getCount());
+        return c1.compareTo(c2);
+        }
 }
--- 02-lab/Generics-Practice03/src/com/example/generics/SortShirtByDesc.java
+++ 02-lab/Generics-Practice03/src/com/example/generics/SortShirtByDesc.java
@@ -4,7 +4,8 @@ import java.util.Comparator;
 
 public class SortShirtByDesc implements Comparator<Shirt> {
 
-    public int compare(Shirt s1, Shirt s2) {
-                return 0;
-    }
+    public int compare(Shirt s1, Shirt s2){
+        return
+        s1.getDescription().compareTo(s2.getDescription());
+        }
 }
